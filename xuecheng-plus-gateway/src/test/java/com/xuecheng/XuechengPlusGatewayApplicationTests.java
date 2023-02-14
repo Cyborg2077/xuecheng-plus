4f4934a204db2e39ee023e095ef889cb011ae8cb
Stan LEE <Cyderpunk2077@gmail.com>
2023-02-14 18:34:44 +0800

    搭建Nacos和网关服务

---
+++ xuecheng-plus-gateway/src/test/java/com/xuecheng/XuechengPlusGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuecheng;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengPlusGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
